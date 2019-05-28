261fec9f28b851bee3e86b638eafc98a9c5c0d51
Endboss <gituser@nothing.com>
2019-05-28 13:46:17 -0700

    changes to travis deploy script to scp built assets via new ssh container into shared volume more directly.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,17 +11,11 @@ set -e
 # copy the result of the build (done during travis automatic execution of npm test)
 # scp -r -i .travis/deploy_rsa dist resume@chws.ca:/home/resume/reactresume/
 
-######
-### What would be even better is if the /shared folder had gooberweb dir and reactresume dir for these files here to end up in.
-########
 tar -czvf resume.tar.gz index.html dist
 # scp -P 10022 -i .travis/deploy_rsa index.html resume@chws.ca:/tmp
-scp -P 10022 resume.tar.gz resume@chws.ca:/home/resume/reactresume/
-scp -P 10022 index.html resume@chws.ca:/home/resume/reactresume/
-scp -P 10022 -r dist resume@chws.ca:/home/resume/reactresume/
-
-#tarball up the git managed files and add the dist dir we just built to that, then compress and copy to server as well.
-# git archive --format=tar -o resume.tar master;
-# tar -uvf resume.tar dist;
-# gzip -f resume.tar;
-# scp resume.tar.gz resume@chws.ca:/home/resume/reactresume/;
+# scp -P 10022 resume.tar.gz resume@chws.ca:/home/resume/reactresume/
+# scp -P 10022 index.html resume@chws.ca:/home/resume/reactresume/
+# scp -P 10022 -r dist resume@chws.ca:/home/resume/reactresume/
+scp -P 10022 resume.tar.gz resume@chws.ca:/shared/resume/
+scp -P 10022 index.html resume@chws.ca:/shared/resume/
+scp -P 10022 -r dist resume@chws.ca:/shared/resume/
