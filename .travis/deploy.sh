6c8e82c235d99f989f5b5c9e3e1e359983a87d5c
Endboss <gituser@nothing.com>
2019-05-27 21:04:25 -0700

    simplify final deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,21 +2,25 @@
 set -e
 # ssh resume@chws.ca -v exit
 
-git config --global push.default simple # we only want to push one branch — master
-# specify the repo on the live server as a remote repo, and name it 'production'
-# <user> here is the separate user you created for deploying
-git remote add production ssh://resume@chws.ca/home/resume/reactresume
-git push production master # push our updates
+# git config --global push.default simple # we only want to push one branch — master
+# # specify the repo on the live server as a remote repo, and name it 'production'
+# # <user> here is the separate user you created for deploying
+# git remote add production ssh://resume@chws.ca/home/resume/reactresume
+# git push production master # push our updates
 
 # copy the result of the build (done during travis automatic execution of npm test)
 # scp -r -i .travis/deploy_rsa dist resume@chws.ca:/home/resume/reactresume/
+
+######
+### What would be even better is if the /shared folder had gooberweb dir and reactresume dir for these files here to end up in.
+########
+tar -czvf resume.tar.gz index.html dist
+scp resume.tar.gz resume@chws.ca:/home/resume/reactresume/
+scp index.html resume@chws.ca:/home/resume/reactresume/
 scp -r dist resume@chws.ca:/home/resume/reactresume/
-# cd ..;
-# pwd;
-# ls -l;
 
 #tarball up the git managed files and add the dist dir we just built to that, then compress and copy to server as well.
-git archive --format=tar -o resume.tar master;
-tar -uvf resume.tar dist;
-gzip -f resume.tar;
-scp resume.tar.gz resume@chws.ca:/home/resume/reactresume/;
+# git archive --format=tar -o resume.tar master;
+# tar -uvf resume.tar dist;
+# gzip -f resume.tar;
+# scp resume.tar.gz resume@chws.ca:/home/resume/reactresume/;
