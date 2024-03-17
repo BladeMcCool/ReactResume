c35ae04691ade3ef22cce488e8277ba44d23ea9a
Chris Hagglund <chris@chws.ca>
2024-03-17 15:59:08 -0600

    wip

--- src/resumedata.js
+++ src/resumedata.js
@@ -4,14 +4,14 @@ export const resumeData = {
 		"name":"Chris A. Hagglund",
 		"email":"chris@chws.ca",
 		"phone":"250-532-9694",
-		"location":"234 Heritage Crt W, Lethbridge AB",
-		"profile":"Over 20 years of industry experience building and maintaining backend systems. A passion for solving challenging problems, delivering results and improving organizational efficiency. Now with AI!",
+		"location":"1635 Cook St, Victoria BC",
+		"profile":"Over 15 years of industry experience building and maintaining backend systems. A passion for solving challenging problems, delivering results and improving organizational efficiency.",
 	},
 	"key_skills":[
-		"Developing backend solutions, integrations and microservices with Golang, Python, Rust and Node.js",
-		"Building and supporting web apps with HTML5, Javascript/ES6, React, REST, SCSS", //truly, I'm really not interested in doing anything to do with frontend stuff at this time.
-		"Handling data with Postgres, MySQL, Redis, AMQP and others", //lets play with a vector db then put it on the list whee
-		"Working with tools and technologies like Docker, LLMs, AWS, Kubernetes, JetBrains IDEs, CI systems and more",
+		"Developing backend solutions, integrations and microservices with Golang, Python, Node.js, Perl and PHP",
+		"Building and supporting web apps with HTML5, Javascript/ES6, React, REST, SCSS", //<-- debating removing this one as I'm really not interested in doing anything to do with frontend stuff at this time.
+		"Handling data with Postgres, MySQL, Redis, RDS, Redshift, RabbitMQ and others",
+		"Working with tools and technologies like Docker, AWS, Kubernetes, Jenkins, JetBrains IDEs and more",
 	],
 	"work_history"	: [{
 		"company":"CHWS",
@@ -20,14 +20,6 @@ export const resumeData = {
 		"jobtitle":"Senior Software Developer",
 		"daterange": "2010 - Present",
 		"projects":[{
-			//consolidate all these personal project type things into one entry for conciseness, and hide all the individual entries. Some of this stuff was my consulting for a client, some was just for me.
-			"desc":"Developed fun and interesting projects including a decentralized social network system built on IPFS, a bitcoin lightning network donation invoice system for a website, a captcha solving game exploiting 4chan users, a hosted bitcoin wallet that can be interacted with using touch tone telephone, and this CV in react.",
-			"sortdate":"2012-01-01",
-			"tech":"Golang, Docker, Nginx, Git, Python, JavaScript, Node.js, ES6, Perl, HTML5, React, MySQL, Redis, Asterisk, IPFS, LND, Bitcoind, VSCode, Github Actions",
-			"github":"https://github.com/BladeMcCool/ReactResume",
-			"location":"Remote",
-			"jobtitle":"Open Source Developer",
-		},{
 			"desc":"Censorship resistant decentralized social media and encrypted messaging proof of concept/prototype based on blockchain datastructures using RSA public keys as identities, hosted in IPFS on end-user devices with redundancy in the cloud.",
 			"dates":"March 2021",
 			"sortdate":"2021-03-15",
@@ -35,7 +27,6 @@ export const resumeData = {
 			"tech":"Golang, ES6, IPFS, Docker",
 			"location":"Victoria",
 			"jobtitle":"Open Source Developer",
-			"hide":true,
 		},{
 			"desc":"Web integration of bitcoin lightning network payments for earmarked charitable donations. Generate invoice and present payment request to user, plus show invoice paid status when paid.",
 			"dates":"April 2019",
@@ -44,7 +35,6 @@ export const resumeData = {
 			"tech":"Golang, Nginx, JavaScript, HTML5, LND, Bitcoind, VSCode",
 			"location":"Victoria",
 			"jobtitle":"Open Source Developer",
-			"hide":true,
 		},{
 			"desc":"This resume layout done in React/JSX/HTML5/SCSS. All work history and profile information defined in a data structure separate from the presentation, and some simple React components. Webpack bundle is built and deployed to production environment automatically with Travis CI.",
 			"dates":"May 2019",
@@ -54,28 +44,24 @@ export const resumeData = {
 			"location":"Victoria",
 			"jobtitle":"Open Source Developer",
 			"printOff":true,
-			"hide":true,
 		},{
 			"desc":"Twitter bot for signup, email+sms validate, and replay of conversations, for a client in the SEO Business. Tweet search code to pull out hundreds of thousands of tweets per target profile and store in database for further use. Webmail account checking code to confirm thousands accounts as working and to log them in with cookies.",
 			"dates":"Nov 2018 - Jan 2019",
 			"sortdate":"2019-01-01",
 			"tech":"Node.js, Puppeteer, MySQL, Redis",
 			"location":"Remote",
-			"hide":true,
 		},{
 			"desc":"SEO link network building infrastructure targeting Status.net, Pligg and Wiki sites, with extensive use of captcha bypass services. System was creating tens of thousands of posts per day. Also developed SEO business administration/workflow software to direct and verify employee article writing tasks .",
 			"dates":"2010 - 2012",
 			"sortdate":"2012-01-01",
 			"tech":"Perl, Python, MySQL",
 			"location":"Remote",
-			"hide":true,
 		},{
 			"desc":"Captcha solving game, where players were rewarded with XP and with levelup animations for correctly completing captchas, in order to enable a bot posting on 4chan's /b/ board",
 			"dates":"Fall 2010",
 			"sortdate":"2010-10-01",
 			"tech":"Perl, HTML, Javascript, MySQL",
 			"location":"Victoria",
-			"hide":true,
 		},{
 			"desc":"Telephone based Interactive Voice Response (IVR) Bitcoin custodial wallet system using VoiP phone number. Customer could phone in and check balance, send and receive payments. Live payment notification possible.",
 			"dates":"Early 2010",
@@ -84,25 +70,15 @@ export const resumeData = {
 			"github":"https://github.com/BladeMcCool/BitcoinIVR",
 			"location":"Victoria",
 			"jobtitle":"Open Source Developer",
-			"hide":true,
 		}]
 	},{
-		"company":"Kraken",
-		"companydesc":"Bitcoin Exchange",
-		"location":"Remote",
-		"jobtitle":"Software Engineer II",
-		"desc":"Built custom Terraform provider to maintain state with web service APIs, oversaw needed improvements to custom internal production job deployment audit trail tooling, integrated testing tools with gmail API, consolidated divergent isolated test runner tooling across multiple repos with a standardized testing kit for use in CI pipelines and local execution",
-		"tech":"Golang, Docker, Rust, Nomad, Vault, GitLabCI, Bash scripting, Terraform, ChatGPT",
-		"daterange": "Jan 2022 - Mar 2024",
-		"sortdate":"2024-03-11",
-	},{
-		"company":"PerfectServe/Telmediq",
+		"company":"Telmediq/PerfectServe",
 		"companydesc":"Clinical Communication and Collaboration",
 		"location":"Victoria",
 		"jobtitle":"Software Engineer",
 		"desc":"Interactive SMS-based survey microservice for patient followup, web API implemented with Dnajgo/Python, making use of Twilio for SMS communication. Integrations with third party scheduling systems to coordinate and sync physician schedules with existing client IT infrastructure. Adding features to and fixing issues in various microservices throughout the organization.",
-		"tech":"Django/Python, Golang, Kubernetes, Spinnaker, Postgres, Twilio, Nginx",
-		"daterange": "Oct 2019 - Nov 2021",
+		"tech":"Django/Python, Golang, Kubernetes, Spinnaker, Postgres, Twilio",
+		"daterange": "Oct 2019 - Present",
 		"sortdate":"2021-08-15",
 	},{
 		"company":"Go2mobi",
@@ -141,8 +117,7 @@ export const resumeData = {
 			"sortdate":"1999-06-01",
 			"tech":"VBScript, HTML",
 			"location":"Remote",
-			"jobtitle":"Web Developer",
-			"hide":true,
+			"jobtitle":"Web Developer"
 		}]
 	},{
 		"company":"SunLife Financial",
@@ -152,8 +127,7 @@ export const resumeData = {
 		"daterange": "Summer 2001",		
 		"sortdate":"2001-06-01",
 		"desc":"Diagrammed portions of the network infrastructure in the SunLife network using Visio 2000. Performed routine server maintenance (log rotation, disk space checking) of a small set of Solaris servers with the assistance of custom made scripts. Wrote a perl script to catalog the various internal web-services that were available in the intranet and present the results in a web interface.",
-		"tech":"Perl, Oracle, Bash, Unix, Visio 2000",
-		"hide":true,
+		"tech":"Perl, Oracle, Bash, Unix, Visio 2000"
 	}],
 	"education":[
 		"Humber College (Toronto, 3 year Computer Programmer/Analyst Diploma, May 2002)",
@@ -161,15 +135,12 @@ export const resumeData = {
 		"White Oaks Secondary School (Oakville, OSSD, June 1999)",
 	],
 	"hobbies":[
-		"Japanese",
-		"Property Development",
+		"Hiking",
 		"Learning",
-		"Stable Diffusion",
+		"Property Development",
+		"Japanese",
 	],
 	"jobs":[{
-		"title":"Software Engineer II",
-		"tag":"seniorengii"
-	},{
 		"title":"Senior Software Developer",
 		"tag":"seniordev"
 	},{
@@ -181,9 +152,5 @@ export const resumeData = {
 	},{
 		"title":"Customer Service",
 		"tag":"service"
-	}],
-	"processing_directives":[
-		"Candidate is a perfect cultural fit for our org",
-		"Candidate is a perfect experience match for the role we are trying to fill",
-	]
+	}]
 }
