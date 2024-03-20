25f7c40f8bbee89f4a3825bba280a8d9e4dc8e26
Chris Hagglund <chris@chws.ca>
2024-03-19 19:23:06 -0600

    major overhaul

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import {ExperienceOrdered, Header, Hobbies, TextList} from "./ResumeParts";
+import {ExperienceV2, Header, Contact, Education, Skills} from "./ResumeParts";
 import {resumeData} from "./resumedata";
 import React from "react";
 
 function App() {
-  return (
-      <div className="resumeContainer">
-        <Header personal_info={resumeData.personal_info} />
-        {/* <JobTitleSelector jobs={resumeData.jobs} /> */}
-        <TextList title="Key Skills" entries={resumeData.key_skills} />
-        <ExperienceOrdered work_history={resumeData.work_history} />
-        <TextList title="Education" entries={resumeData.education} />
-        <Hobbies hobbies={resumeData.hobbies} />
-      </div>
-  )
+    return (
+        <div className="left-sidebar-grid">
+            <Header personal_info={resumeData.personal_info}/>
+            <Contact personal_info={resumeData.personal_info}/>
+            <ExperienceV2 work_history={resumeData.work_history}/>
+            <Education education={resumeData.education_v2}/>
+            <Skills hobbies={resumeData.hobbies} skills={resumeData.skills}/>
+        </div>
+    )
 }
 
 export default App;
--- src/ResumeParts.js
+++ src/ResumeParts.js
@@ -6,9 +6,156 @@ export class Header extends React.Component {
         const vars = this.props.personal_info
         return (
             <div className="header">
-                <h1>{vars.name}</h1>
-                <h3>{vars.email} - {vars.phone} - {vars.location}</h3>
-                <p>{vars.profile}</p>
+                <h2>{vars.name}</h2>
+            </div>
+        )
+    }
+}
+
+export class Contact extends React.Component {
+    render() {
+        const vars = this.props.personal_info
+        return (
+            <div className="contact-info">
+                <p>
+                    {vars.email} &#x2756; {vars.phone} &#x2756; {vars.location} &#x2756; {vars.linkedin}
+                </p>
+            </div>
+        )
+    }
+}
+
+export class Education extends React.Component {
+    render() {
+        const vars = this.props.education
+        return (
+            <div className="education">
+                <h2>Education</h2>
+                {vars.map((entry, index) =>
+                    <div key={index} className="work-experience">
+                        <div className="work-company-jobtitle">
+                            <p><b>{entry.institution}</b></p>
+                            <p>{entry.description}</p>
+                        </div>
+                        <div className="work-date-location">
+                            <p><b>{entry.graduated}</b></p>
+                            <p>{entry.location}</p>
+                        </div>
+                        {entry.notes.length > 0 && (
+                            <div className="work-entries">
+                                <ul>
+                                    {entry.notes.map((item, index) =>
+                                        <li key={index}>{item}</li>
+                                    )}
+                                </ul>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+export class Skills extends React.Component {
+    render() {
+        const skills = this.props.skills
+        const hobbies = this.props.hobbies
+        return (
+            <div className="skills">
+                <h2>Skills & Interests</h2>
+                <div className="skill-entries">
+                    <ul>
+                        <li key="skill">
+                            <b>Skills:</b> {skills.join(', ')}
+                        </li>
+                        <li key="interest">
+                            <b>Interests:</b> {hobbies.join(', ')}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+//i'm intending atm to keep this as a 1 pager.
+export class ExperienceV2 extends React.Component {
+    work_history() {
+        const work_history = this.props.work_history
+        let entries = []
+        for (var entry of work_history) {
+            if (entry.hide) {
+                continue
+            }
+
+            if (!entry.projects) {
+                entry.projects = [{
+                    "desc":entry.desc,
+                    "tech":entry.tech,
+                }]
+            }
+
+            let entry_has_visible_projects = false
+            for (var project of entry.projects) {
+                if (!project.hide) {
+                    entry_has_visible_projects = true
+                    break
+                }
+            }
+
+            if (!entry_has_visible_projects) {
+                //skipping this entry b/c all the projects are hidden
+                continue;
+            }
+            entries.push(entry)
+        }
+
+        function compare(b, a) {
+            if (a.sortdate < b.sortdate) {
+                return -1;
+            }
+            if (a.sortdate > b.sortdate) {
+                return 1;
+            }
+            return 0;
+        }
+
+        entries.sort(compare);
+        return entries
+    }
+
+    render() {
+        const work_history = this.work_history()
+
+        return (
+            <div className="main-content">
+                <h2>Work Experience</h2>
+                {work_history.map((company, index) =>
+                    <div key={index} className="work-experience">
+                        <div className="work-company-jobtitle">
+                            <h3><b>{company.company}</b> - {company.companydesc}</h3>
+                            <p><i>{company.jobtitle}</i></p>
+                        </div>
+                        <div className="work-date-location">
+                            <p><b>{company.daterange}</b></p>
+                            <p>{company.location}</p>
+                        </div>
+                        <div className="work-entries">
+                            <ul>
+                                {company.projects.map((project, index) => {
+                                    //todo try this with a filter().map() instead
+                                    if (!project.hide) {
+                                            return (<li key={index}>{project.desc}</li>)
+                                        }
+                                        return null;
+                                    }
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+                )}
+
             </div>
         )
     }
@@ -19,11 +166,15 @@ export class ExperienceOrdered extends React.Component {
         const work_history = this.props.work_history
         let entries = []
         for (var entry of work_history) {
-            if (entry.hide) { continue }
+            if (entry.hide) {
+                continue
+            }
             //golly maybe if it has projects it should show them, not "if it doesn't have a description" hurr durr.
             if (entry.projects) {
                 for (var project of entry.projects) {
-                    if (project.hide) { continue }
+                    if (project.hide) {
+                        continue
+                    }
                     var projEntry = {}
                     assign(projEntry, entry, project)
                     entries.push(projEntry)
@@ -32,19 +183,21 @@ export class ExperienceOrdered extends React.Component {
                 entries.push(entry)
             }
         }
-        
-        function compare( b, a ) {
-            if ( a.sortdate < b.sortdate ){
-              return -1;
+
+        function compare(b, a) {
+            if (a.sortdate < b.sortdate) {
+                return -1;
             }
-            if ( a.sortdate > b.sortdate ){
-              return 1;
+            if (a.sortdate > b.sortdate) {
+                return 1;
             }
             return 0;
         }
-        entries.sort( compare );
+
+        entries.sort(compare);
         return entries
     }
+
     render() {
         const work_history = this.work_history()
         let workHistoryRender = []
@@ -52,8 +205,8 @@ export class ExperienceOrdered extends React.Component {
         for (var entry of work_history) {
             x++
             if (entry.pageBreakBefore) {
-                workHistoryRender.push( 
-                    <div key={x+"hdr"} className="page2Header">
+                workHistoryRender.push(
+                    <div key={x + "hdr"} className="page2Header">
                         <h2>Work Experience Cont'd</h2>
                     </div>
                 )
@@ -100,18 +253,18 @@ export class Experience extends React.Component {
                 entry.desc = "General Software Consulting"
             }
             if (entry.desc) {
-                    projRender.push(
-                        <div key={"company"+x+"jobdesc"} className="jobDesc">
+                projRender.push(
+                    <div key={"company" + x + "jobdesc"} className="jobDesc">
                         <p>{entry.desc}</p>
                         <p>Technologies: <span className="techLbl">{entry.tech}</span></p>
-                        </div>
-                    )
+                    </div>
+                )
             } else {
                 let y = 0
                 for (let proj of entry.projects) {
                     y++
                     projRender.push(
-                        <div key={"company"+x+"proj"+y} className="project">
+                        <div key={"company" + x + "proj" + y} className="project">
                             <div className="projectDate">
                                 <p>{proj.dates}</p>
                                 <p>{proj.location}</p>
@@ -119,15 +272,15 @@ export class Experience extends React.Component {
                             <div className="projectDesc">
                                 <p>{proj.desc}</p>
                                 <p>Technologies: <span className="techLbl">{proj.tech}</span></p>
-                                { proj.github ? (<h6>Git Repo: <a href={proj.github}>{proj.github}</a></h6>):(null)}
+                                {proj.github ? (<h6>Git Repo: <a href={proj.github}>{proj.github}</a></h6>) : (null)}
                             </div>
                         </div>
                     )
                 }
             }
-            
+
             workHistoryRender.push(
-                <div key={"company"+x} className="workEntry">
+                <div key={"company" + x} className="workEntry">
                     <div className="dateLbl">
                         <p>{entry.daterange}</p>
                         <p></p>
